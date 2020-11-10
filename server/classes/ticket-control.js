5b7906d97fd2d54468320229846336004ee2212c
Miguel García Reimers <miguelgarciareimers@Miguels-MacBook-Pro.local>
2020-11-10 20:07:10 +0100

    New tickets page developed.

--- server/classes/ticket-control.js
+++ server/classes/ticket-control.js
@@ -5,11 +5,13 @@ class TicketControl {
     constructor() {
         this.lastTicket = 0;
         this.today = new Date().getDate();
+        this.tickets = [];
 
         const data = require('../data/data.json');
 
         if (data.today === this.today) {
             this.lastTicket = data.lastTicket;
+            this.tickets = data.tickets;
         } else {
             this.resetData();
         }
@@ -18,12 +20,15 @@ class TicketControl {
     // Reset data.
     resetData = () => {
         this.lastTicket = 0;
+        this.tickets = [];
         this.saveFile();
     }
 
     // Add 1 to last ticket.
     nextTicket = () => {
         this.lastTicket++;
+        this.tickets.push(new Ticket(this.lastTicket, null));
+
         this.saveFile();
 
         return `Ticket ${ this.lastTicket }`;
@@ -31,13 +36,20 @@ class TicketControl {
 
     // Save file.
     saveFile = () => {
-        const jsonData = { 'lastTicket': this.lastTicket, 'today': this.today };
+        const jsonData = { lastTicket: this.lastTicket, tickets: this.tickets, today: this.today };
         fs.writeFileSync('./server/data/data.json', JSON.stringify(jsonData));
     }
 
     // Get last ticket.
     getLastTicket() {
         return `Ticket ${ this.lastTicket }`;
+    }
+}
+
+class Ticket {
+    constructor(number, desktop) {
+        this.number = number;
+        this.desktop = desktop;
     }
 }
 
